c0d941dbc12c1b311474757ce8e8e13db7db3875
Matias <matias.miguenz@etermax.com>
2022-02-13 15:44:09 -0300

    lottery first deploy

---
+++ README.md
@@ -0,0 +1,9 @@
+1. Users can enter lottery with ETH based on a USD fee
+2. An Admin will choose when the lottery is over
+3. The lotter will select a random winner
+
+How do we want to test this?
+
+1. `mainet-fork`
+2. `development with mocks`
+3. `testnet`
---
+++ contracts/Lottery.sol
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+
+contract Lottery is VRFConsumerBase, Ownable {
+    address payable[] public players;
+    uint256 public usdEntryFee;
+    address payable public recentWinner;
+    uint256 public randomness;
+
+    AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+
+    LOTTERY_STATE public lottery_state;
+    uint256 public fee;
+    bytes32 public keyHash;
+
+    constructor(
+        address _priceFeedAddress,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyHash
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
+        usdEntryFee = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        fee = _fee;
+        keyHash = _keyHash;
+    }
+
+    function enter() public payable {
+        // $50 minimum
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10**10; // 18 decimals
+
+        // $50, $2,000 / eth
+        // $50 /2,000
+        // 50 * 100000 / 2000
+
+        uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
+        return costToEnter;
+    }
+
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "Can't start a new lottery yet!"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
+
+    function endLottery() public onlyOwner {
+        /*   uint256(
+            keccack256(
+                abi.encodePacked(
+                    nonce, // nonce is predictable (aka, transaction number)
+                    msg.sender, // msg.sender is predictable
+                    block.difficulty, // can actually be manipulated by the miners
+                    block.timestamp // timestamp is predictable
+                )
+            )
+        ) % players.length;
+    */
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requesstId = requestRandomness(keyHash, fee);
+    }
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "You aren't there yet!"
+        );
+
+        require(_randomness > 0, "random-not-found");
+
+        uint256 indexWinner = _randomness % players.length;
+
+        recentWinner = players[indexWinner];
+
+        recentWinner.transfer(address(this).balance);
+
+        // reset
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        randomness = _randomness;
+    }
+}
